821cb6699bb939019b4dd69c9dc16eb4a5f068e8
robinzon <robinzzzzzon@gmail.com>
2022-11-11 01:06:21 +0300

    check master protection

---
+++ source/check.js
@@ -0,0 +1,26 @@
+import './styles/indexStyles.css'
+import './styles/commonStyles.css'
+const NewDictionaryPage = require('./pages/NewDictionaryPage')
+const StudyDictionaryPage = require('./pages/StudyDictionaryPage')
+
+const actionRoot = document.querySelector('.actionRoot')
+actionRoot.addEventListener('click', renderIndexPage)
+
+if (!localStorage.length) {
+  const trainBtn = document.querySelector('[data-name="train"]')
+  trainBtn.disabled = 'true'
+}
+
+export function renderIndexPage(event) {
+  event.preventDefault()
+
+  if (!event.target.dataset.name) return
+
+  const name = event.target.dataset.name
+
+  if (name === 'study') {
+    NewDictionaryPage.renderNewDictionaryPage()
+  } else if (name === 'train') {
+    StudyDictionaryPage.renderStudyDictionaryPage()
+  }
+}
