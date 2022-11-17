893d16094b77df16b13ff3f347303608e997be0d
robinzon <robinzzzzzon@gmail.com>
2022-11-17 22:07:03 +0300

    add compare word list check

--- source/pages/NewDictionaryPage.js
+++ source/pages/NewDictionaryPage.js
@@ -1,24 +1,28 @@
 const LookThroughPage = require('./LookThroughPage')
+const dictionary = require('../dictionary.json')
+const utils = require('../utils')
 
 const content = document.querySelector('.content')
+const speechList = ['verbs', 'illegal verbs', 'phrazal verbs', 'nouns', 'adjectives', 'adverbs', 'pronouns', 'numerals', 'other parts', 'idioms']
 
 export function renderNewDictionariesPage() {
-  content.innerHTML = `
-        <div class="dictionaryRoot">
-            <div class="dictionary" data-name="verbs">VERBS</div>
-            <div class="dictionary" data-name="illegal verbs">ILLEGAL VERBS</div>
-            <div class="dictionary" data-name="phrazal verbs">PHRAZAL VERBS</div>
-            <div class="dictionary" data-name="nouns">NOUNS</div>
-            <div class="dictionary" data-name="adjectives">ADJECTIVES</div>
-            <div class="dictionary" data-name="adverbs">ADVERBS</div>
-            <div class="dictionary" data-name="pronouns">PRONOUNS</div>
-            <div class="dictionary" data-name="numerals">NUMERALS</div>
-            <div class="dictionary" data-name="other parts">OTHER PARTS</div>
-            <div class="dictionary" data-name="idioms">IDIOMS</div>
-        </div>
-    `
+  content.innerHTML = `<div class="dictionaryRoot"></div>`
 
   const dictionaryRoot = document.querySelector('.dictionaryRoot')
+
+  for (let index = 0; index < speechList.length; index++) {
+    const item = document.createElement('button')
+    item.classList.add('dictionary')
+    item.setAttribute('data-name', speechList[index])
+    item.textContent = speechList[index].toUpperCase()
+
+    const dictionaryList = dictionary.filter((item) => item.wordType === speechList[index])
+    const storageList = utils.getWordsFromStorage(speechList[index])
+
+    if (dictionaryList.length === storageList.length) item.disabled = 'true'
+
+    dictionaryRoot.append(item)
+  }
 
   dictionaryRoot.addEventListener('click', (event) => {
     event.preventDefault()
