ee7b55d8480c83d708548fa51c6118c412a88494
Feelmax <feelmax@Feelmaxs-MacBook-Pro.local>
2024-11-14 23:45:44 +0400

    fix

--- wdio.shared_concurrent.conf.mjs
+++ wdio.shared_concurrent.conf.mjs
@@ -2,6 +2,6 @@ import wdioConstants from './wdio.constants.mjs'
 
 const specPath = process.env.CI_MERGE_REQUEST_TITLE 
 ? [`${wdioConstants.basePath}/*smoke*.spec.mjs`] 
-: [`./source/tests/specs/*.spec.mjs`]
+: [`${wdioConstants.basePath}/*.spec.mjs`]
 
 export const concurrentConfig = wdioConstants.giveCapsList(specPath, 1, 1)
--- wdio.shared_parallel.conf.mjs
+++ wdio.shared_parallel.conf.mjs
@@ -2,6 +2,6 @@ import wdioConstants from './wdio.constants.mjs'
 
 const specPath = process.env.CI_MERGE_REQUEST_TITLE 
 ? [`${wdioConstants.basePath}/parallel/*smoke*.spec.mjs`] 
-: [`./source/tests/specs/parallel/*.spec.mjs`]
+: [`${wdioConstants.basePath}/parallel/*.spec.mjs`]
 
 export const parallelConfig = wdioConstants.giveCapsList(specPath, 2, 2)
