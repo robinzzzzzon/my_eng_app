2b999ba1668bc45af6da39e64ac33a9e08402367
Feelmax <feelmax@Feelmaxs-MacBook-Pro.local>
2024-12-08 00:21:22 +0400

    add 2nd part of actions

--- .github/actions/deploy-actions/main.js
+++ .github/actions/deploy-actions/main.js
@@ -2,8 +2,15 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const exec = require('@actions/exec')
 
-function printSmth() {
-    core.notice('Hello from main.js!')
+function run() {
+    // Get input values:
+    const bucket = core.getInput('bucket', { required: true })
+    const bucketRegion = core.getInput('bucket-region', { required: true })
+    const distFolder = core.getInput('dist-folder', { required: true })
+
+    // Upload files:
+    const uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${uri} --region ${bucketRegion}`)
 }
 
-printSmth()
+run()
